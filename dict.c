3bdb9239abf50dcdab357328afd7624c3bbb6450
Joe Futrelle <joefutrelle@gmail.com>
2000-08-08 20:35:12 +0000

    added traversal routine, fixed major memory leak in dict_freeValues

--- dict.c
+++ dict.c
@@ -18,6 +18,17 @@ DICT_ENTRY *dictEntry_new(char *key, void *value) {
      return nde;
 }
 
+void dictEntry_free(DICT_ENTRY *entry) {
+     free(entry->key);
+     free(entry);
+}
+
+void dictEntry_freeData(DICT_ENTRY *entry, Destructor d) {
+     d(entry->value);
+     free(entry->key);
+     free(entry);
+}
+
 DICT *dict_new() {
      DICT *nd = calloc(1,sizeof(DICT));
      nd->entries = list_new();
@@ -27,10 +38,16 @@ DICT *dict_new() {
 void dict_free(DICT *d) {
      /* recursively free a DICT */
      int i;
+     long il, len;
      for(i = 0; i < 26; i++) {
 	  if(d->children[i]) {
 	       dict_free(d->children[i]);
 	  }
+     }
+     len = list_length(d->entries);
+     for(il = 0; il < len; il++) {
+	  DICT_ENTRY *e = (DICT_ENTRY *)list_get(d->entries,il);
+	  dictEntry_free(e);
      }
      list_free(d->entries);
      free(d);
@@ -39,12 +56,18 @@ void dict_free(DICT *d) {
 void dict_freeValues(DICT *d, Destructor destructo) {
      /* recursively free a DICT */
      int i;
+     long il, len;
      for(i = 0; i < 26; i++) {
 	  if(d->children[i]) {
 	       dict_freeValues(d->children[i], destructo);
 	  }
      }
-     list_freeData(d->entries, destructo);
+     len = list_length(d->entries);
+     for(il = 0; il < len; il++) {
+	  DICT_ENTRY *e = (DICT_ENTRY *)list_get(d->entries,il);
+	  dictEntry_freeData(e, destructo);
+     }
+     list_free(d->entries);
      free(d);
 }
 
@@ -57,11 +80,13 @@ void _dict_put(DICT *d, char *key, char *orig_key, void *data) {
      /* if the key is empty, we've found or created the appropriate
 	leaf node and now just need to append an entry to it */
      if(!*kp) {
+	  DICT_ENTRY *newEntry;
+	  newEntry = dictEntry_new(orig_key, data);
 #ifdef DEBUG
-	  printf("adding new entry %s\n", orig_key);
+	  printf("adding new entry %s (%p)\n", orig_key, newEntry);
 	  fflush(stdout);
 #endif
-	  list_add(d->entries,dictEntry_new(orig_key,data));
+	  list_add(d->entries, newEntry);
 	  return;
      }
      /* otherwise we need to skip non-alpha chars */
@@ -99,9 +124,6 @@ void *_dict_get(DICT *d, char *key, char *orig_key) {
      while(*kp && !isalpha(*kp)) {
 	  kp++;
      }
-#ifdef DEBUG
-     printf("getting %s\n",kp); /* debug */
-#endif
      if(!*kp) {
 	  /* do a linear search of this node's entries */
 	  int i, len;
@@ -118,6 +140,36 @@ void *_dict_get(DICT *d, char *key, char *orig_key) {
 	  }
 	  return NULL;
      }
+#ifdef DEBUG
+     printf("getting %s\n",kp); /* debug */
+#endif
+     ix = tolower(*kp) - 'a';
+     if(!d->children[ix])
+	  return NULL;
+     return _dict_get(d->children[ix], kp+1, orig_key);
+}
+
+LIST *_dict_getAll(DICT *d, char *key, char *orig_key) {
+     char *kp = key;
+     int ix;
+     /* skip nonalphas */
+     while(*kp && !isalpha(*kp)) {
+	  kp++;
+     }
+#ifdef DEBUG
+     printf("getting %s\n",kp); /* debug */
+#endif
+     if(!*kp) {
+	  LIST *all;
+	  int i, len;
+	  all = list_new();
+	  len = list_length(d->entries);
+	  for(i = 0; i < len; i--) {
+	       DICT_ENTRY *de = (DICT_ENTRY *)list_get(d->entries,i);
+	       list_add(all, de->value);
+	  }
+	  return all;
+     }
      ix = tolower(*kp) - 'a';
      if(!d->children[ix])
 	  return NULL;
@@ -127,3 +179,31 @@ void *_dict_get(DICT *d, char *key, char *orig_key) {
 void *dict_get(DICT *d, char *key) {
      return _dict_get(d, key, key);
 }
+
+LIST *dict_getAll(DICT *d, char *key) {
+     return _dict_get(d, key, key);
+}
+
+void dict_traverse(DICT *d, TraversalAction action, void *action_arg) {
+     int i;
+     if(!d)
+	  return;
+     if(d->entries) {
+	  int len;
+	  len = list_length(d->entries);
+#ifdef DEBUG
+	  printf("%d entries\n", len);
+#endif
+	  for(i = 0; i < len; i++) {
+	       DICT_ENTRY *de = (DICT_ENTRY *)list_get(d->entries,i);
+#ifdef DEBUG
+	       printf("entry %d = %p\n", i, de);
+#endif
+	       action(de->key, de->value, action_arg);
+	  }
+     }
+     for(i = 0; i < 26; i++)
+	  if(d->children[i])
+	       dict_traverse(d->children[i], action, action_arg);
+}
+
--- dict.h
+++ dict.h
@@ -6,8 +6,11 @@
  * it has exactly the same API as the dict module and can be
  * used in its place
  */
+
 #include "list.h"
 #include "mem.h"
+
+typedef void (*TraversalAction)(char *key, void *value, void *arg);
 
 typedef struct _dict_entry {
      char *key;
@@ -25,5 +28,11 @@ extern void dict_freeValues(DICT *, Destructor);
 
 extern void dict_put(DICT *, char *key, void *data);
 extern void *dict_get(DICT *, char *key);
+
+/*
+ * additional trie-only functions
+ */
+extern LIST *dict_getAll(DICT *, char *);
+extern void dict_traverse(DICT *, TraversalAction, void *);
 
 #endif
